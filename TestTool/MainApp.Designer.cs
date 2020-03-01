bdedc2b73569eeeb8f9386467ea95d1a9f748c52
lehoai214 <lehoai.214@gmail.com>
2020-03-01 16:47:16 +0900

    Add project files.

---
+++ TestTool/MainApp.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace TestTool
+{
+    partial class MainApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.MainTab = new System.Windows.Forms.TabPage();
+            this.BtnGo = new System.Windows.Forms.Button();
+            this.TxtPage = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BtnNext = new System.Windows.Forms.Button();
+            this.BtnPre = new System.Windows.Forms.Button();
+            this.TxtRows = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.GridViewData = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TxtFileSize = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LblLines = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnSelectFile = new System.Windows.Forms.Button();
+            this.TxtFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.FileOpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.MainTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewData)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.MainTab);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(16, 11);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1583, 817);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // MainTab
+            // 
+            this.MainTab.Controls.Add(this.BtnGo);
+            this.MainTab.Controls.Add(this.TxtPage);
+            this.MainTab.Controls.Add(this.label7);
+            this.MainTab.Controls.Add(this.BtnNext);
+            this.MainTab.Controls.Add(this.BtnPre);
+            this.MainTab.Controls.Add(this.TxtRows);
+            this.MainTab.Controls.Add(this.label6);
+            this.MainTab.Controls.Add(this.GridViewData);
+            this.MainTab.Controls.Add(this.panel1);
+            this.MainTab.Controls.Add(this.BtnSelectFile);
+            this.MainTab.Controls.Add(this.TxtFile);
+            this.MainTab.Controls.Add(this.label1);
+            this.MainTab.Location = new System.Drawing.Point(4, 25);
+            this.MainTab.Margin = new System.Windows.Forms.Padding(0);
+            this.MainTab.Name = "MainTab";
+            this.MainTab.Padding = new System.Windows.Forms.Padding(4);
+            this.MainTab.Size = new System.Drawing.Size(1575, 788);
+            this.MainTab.TabIndex = 0;
+            this.MainTab.Text = "Read File";
+            this.MainTab.UseVisualStyleBackColor = true;
+            // 
+            // BtnGo
+            // 
+            this.BtnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnGo.Location = new System.Drawing.Point(1497, 214);
+            this.BtnGo.Name = "BtnGo";
+            this.BtnGo.Size = new System.Drawing.Size(52, 23);
+            this.BtnGo.TabIndex = 11;
+            this.BtnGo.Text = "Go";
+            this.BtnGo.UseVisualStyleBackColor = true;
+            this.BtnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // TxtPage
+            // 
+            this.TxtPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtPage.Location = new System.Drawing.Point(1434, 214);
+            this.TxtPage.Name = "TxtPage";
+            this.TxtPage.Size = new System.Drawing.Size(44, 22);
+            this.TxtPage.TabIndex = 10;
+            this.TxtPage.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1360, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Page : ";
+            // 
+            // BtnNext
+            // 
+            this.BtnNext.Location = new System.Drawing.Point(317, 211);
+            this.BtnNext.Name = "BtnNext";
+            this.BtnNext.Size = new System.Drawing.Size(75, 23);
+            this.BtnNext.TabIndex = 8;
+            this.BtnNext.Text = "Next >>";
+            this.BtnNext.UseVisualStyleBackColor = true;
+            this.BtnNext.Click += new System.EventHandler(this.BtnNext_Click);
+            // 
+            // BtnPre
+            // 
+            this.BtnPre.Location = new System.Drawing.Point(213, 211);
+            this.BtnPre.Name = "BtnPre";
+            this.BtnPre.Size = new System.Drawing.Size(75, 23);
+            this.BtnPre.TabIndex = 7;
+            this.BtnPre.Text = "<< Prev";
+            this.BtnPre.UseVisualStyleBackColor = true;
+            this.BtnPre.Click += new System.EventHandler(this.BtnPre_Click);
+            // 
+            // TxtRows
+            // 
+            this.TxtRows.Location = new System.Drawing.Point(89, 211);
+            this.TxtRows.Name = "TxtRows";
+            this.TxtRows.Size = new System.Drawing.Size(100, 22);
+            this.TxtRows.TabIndex = 6;
+            this.TxtRows.Text = "100";
+            this.TxtRows.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtRows_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Rows : ";
+            // 
+            // GridViewData
+            // 
+            this.GridViewData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GridViewData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridViewData.Location = new System.Drawing.Point(32, 254);
+            this.GridViewData.Name = "GridViewData";
+            this.GridViewData.Size = new System.Drawing.Size(1517, 509);
+            this.GridViewData.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.TxtFileSize);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.LblLines);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(32, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1517, 124);
+            this.panel1.TabIndex = 3;
+            // 
+            // TxtFileSize
+            // 
+            this.TxtFileSize.AutoSize = true;
+            this.TxtFileSize.Location = new System.Drawing.Point(93, 49);
+            this.TxtFileSize.Name = "TxtFileSize";
+            this.TxtFileSize.Size = new System.Drawing.Size(92, 16);
+            this.TxtFileSize.TabIndex = 3;
+            this.TxtFileSize.Text = "0 MB (0 bytes)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "FileSize :";
+            // 
+            // LblLines
+            // 
+            this.LblLines.AutoSize = true;
+            this.LblLines.Location = new System.Drawing.Point(93, 16);
+            this.LblLines.Name = "LblLines";
+            this.LblLines.Size = new System.Drawing.Size(39, 16);
+            this.LblLines.TabIndex = 1;
+            this.LblLines.Text = "0 line";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lines : ";
+            // 
+            // BtnSelectFile
+            // 
+            this.BtnSelectFile.Location = new System.Drawing.Point(648, 17);
+            this.BtnSelectFile.Name = "BtnSelectFile";
+            this.BtnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.BtnSelectFile.TabIndex = 2;
+            this.BtnSelectFile.Text = "Browse...";
+            this.BtnSelectFile.UseVisualStyleBackColor = true;
+            this.BtnSelectFile.Click += new System.EventHandler(this.BtnSelectFile_Click);
+            // 
+            // TxtFile
+            // 
+            this.TxtFile.Location = new System.Drawing.Point(86, 17);
+            this.TxtFile.Name = "TxtFile";
+            this.TxtFile.Size = new System.Drawing.Size(544, 22);
+            this.TxtFile.TabIndex = 1;
+            this.TxtFile.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtFile_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(1575, 788);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Compare File";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // FileOpenDialog
+            // 
+            this.FileOpenDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.FileOpenDialog_FileOk);
+            // 
+            // MainApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1605, 834);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TestTool";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainApp_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainApp_DragEnter);
+            this.tabControl1.ResumeLayout(false);
+            this.MainTab.ResumeLayout(false);
+            this.MainTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewData)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage MainTab;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog FileOpenDialog;
+        private System.Windows.Forms.TextBox TxtFile;
+        private System.Windows.Forms.Button BtnSelectFile;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblLines;
+        private System.Windows.Forms.Label TxtFileSize;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView GridViewData;
+        private System.Windows.Forms.TextBox TxtRows;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnNext;
+        private System.Windows.Forms.Button BtnPre;
+        private System.Windows.Forms.TextBox TxtPage;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button BtnGo;
+    }
+}
+
---
+++ TestTool/MainApp.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestTool
+{
+    public partial class MainApp : Form
+    {
+
+        private int currentPage = 1;
+
+        public MainApp()
+        {
+            InitializeComponent();
+            GridViewData.DoubleBuffered(true);
+            AllowDrop = true;
+        }
+
+        #region READ FILE
+
+        private void BtnSelectFile_Click(object sender, EventArgs e)
+        {
+            FileOpenDialog.ShowDialog();
+        }
+
+        private async void FileOpenDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            TxtFile.Text = FileOpenDialog.FileName;
+            currentPage = 1;
+            readFileLines();
+            await loadReadFileData();
+        }
+
+        private async void TxtFile_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                await loadReadFileData();
+            }
+        }
+
+        private void readFileLines()
+        {
+            LblLines.Text = "Counting...";
+            FileStream fs = new FileStream(TxtFile.Text, FileMode.Open, FileAccess.Read, FileShare.None, 1024 * 1024);
+
+            long lineCount = 0;
+            byte[] buffer = new byte[1024 * 1024];
+            int bytesRead;
+
+            do
+            {
+                bytesRead = fs.Read(buffer, 0, buffer.Length);
+                for (int i = 0; i < bytesRead; i++)
+                    if (buffer[i] == '\n')
+                        lineCount++;
+            }
+            while (bytesRead > 0);
+            LblLines.Text = String.Format("{0:n0}", lineCount) + " line includes header";
+            fs.Close();
+        }
+
+        private async Task loadReadFileData()
+        {
+            FileInfo fileInfo = new FileInfo(TxtFile.Text);
+            double size = fileInfo.Length;
+            TxtFileSize.Text = Math.Round((size / 1024 / 1024), 2) + " MB (" + size + " bytes)";
+
+            // read lines
+            int pageSize = int.Parse(TxtRows.Text);
+            using (var reader = File.OpenText(TxtFile.Text))
+            {
+                // read header
+                String line = await reader.ReadLineAsync();
+                if (line == null)
+                {
+                    return;
+                }
+                string[] headers = line.Split(new char[] { ',' });
+                GridViewData.Columns.Clear();
+                GridViewData.Columns.Add("Line", "Line");
+                foreach (string header in headers)
+                {
+                    GridViewData.Columns.Add(header, header);
+                }
+
+                int currentRow = 0;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    currentRow++;
+                    if (currentRow >= pageSize * currentPage)
+                    {
+                        break;
+                    }
+                    if ((currentPage - 1) * pageSize <= currentRow)
+                    {
+                        string[] values = line.Split(new char[] { ',' });
+                        var list = values.ToList();
+                        list.Insert(0, currentRow.ToString());
+                        values = list.ToArray();
+                        GridViewData.Rows.Add(values);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        private async void MainApp_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            TxtFile.Text = files[0];
+            currentPage = 1;
+            readFileLines();
+            await loadReadFileData();
+        }
+
+        private void MainApp_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private async void BtnNext_Click(object sender, EventArgs e)
+        {
+            currentPage++;
+            await loadReadFileData();
+        }
+
+        private async void BtnPre_Click(object sender, EventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                await loadReadFileData();
+            }
+        }
+
+        private async void TxtRows_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                this.currentPage = 1;
+                await loadReadFileData();
+            }
+        }
+
+        private async void BtnGo_Click(object sender, EventArgs e)
+        {
+            currentPage = int.Parse(TxtPage.Text);
+            await loadReadFileData();
+        }
+    }
+}
